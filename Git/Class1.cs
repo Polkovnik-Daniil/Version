13861abf1a7ccf5c5f8a6b199df992c51a9b585b
Polkovnik-Daniil <0205danik@mail.ru>
2020-09-09 19:24:03 +0300

    Second commit

--- Git/Class1.cs
+++ Git/Class1.cs
@@ -13,8 +13,8 @@ namespace Git
             string hello = "Привет мир";
             Console.WriteLine(hello);
             Console.WriteLine("Добро пожаловать в C#!");
-            //Console.WriteLine("Пока мир...");
-            //Console.WriteLine(24.5);
+            Console.WriteLine("Пока мир...");
+            Console.WriteLine(24.5);
             //Console.WriteLine("Add new information 1");
             //Console.ReadKey();
         }
