bd8b42536f2168f201939a71926f5ff251e67170
Polkovnik-Daniil <0205danik@mail.ru>
2020-09-09 19:23:35 +0300

    first commit

---
+++ Git/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Привет мир";
+            Console.WriteLine(hello);
+            Console.WriteLine("Добро пожаловать в C#!");
+            //Console.WriteLine("Пока мир...");
+            //Console.WriteLine(24.5);
+            //Console.WriteLine("Add new information 1");
+            //Console.ReadKey();
+        }
+    }
+}
